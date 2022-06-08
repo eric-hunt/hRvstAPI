6428dc9db30a84dae0fd301380f551af0ba752b9
Eric Hunt <52219938+eric-hunt@users.noreply.github.com>
2022-06-08 11:04:41 -0400

    added base url and user agent (eric-hunt/hRvstAPI) to package environment

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,5 @@
 .onLoad <- function(libname, pkgname) {
   assign("keyring_service", "Harvest (hRvstAPI)", envir = topenv())
+  assign("base_url", "https://api.harvestapp.com/v2/", envir = topenv())
+  assign("user_agent", "https://github.com/eric-hunt/hRvstAPI", envir = topenv())
 }
